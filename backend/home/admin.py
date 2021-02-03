b4d8575498d511ea4c7fb78eb614f89aefacfb29
Crowdbotics <team@crowdbotics.com>
2021-02-03 16:32:47 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Test2
+
+admin.site.register(Test2)
 
 # Register your models here.
--- backend/lizz_figma_import_2_24198/wsgi.py
+++ backend/lizz_figma_import_2_24198/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lizz_figma_import_2_24198.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lizz_figma_import_2_24198.settings")
 
 application = get_wsgi_application()
